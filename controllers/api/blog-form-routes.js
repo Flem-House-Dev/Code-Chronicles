ddab807c190d56506ed4c39725d32e72901cb079
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-06-13 08:56:21 -0500

    added functionality to save blog posts

--- controllers/api/blog-form-routes.js
+++ controllers/api/blog-form-routes.js
@@ -1,21 +1,42 @@
 const router = require('express').Router();
 const { Blog } = require('../../models');
 
-
+function getCategoryId (category) {
+    switch(category) {
+        case 'HTML':
+            return 1;
+        case 'JavaScript':
+            return 2;
+        case 'CSS':
+            return 3;
+        case 'Node.js':
+            return 4;
+        default:
+            return null;
+    }
+}
 
 router.post('/', async (req, res) => {
     try {
-        const newBlogData = await Blog.create({
-            title: req.body.title,
-            category_id: req.body.category_id,
-            author: req.body.author,
-            text_body: req.body.text_body,
-        });
+        const { title, author, text_body, category } = req.body;
+        const category_id = getCategoryId(category);
 
-        res.status(200).json(newBlogData);
+        if (category_id === null) {
+            return res.status(400).json({ error: 'Invalid category' });
+        }
+        
+        const newBlogPost = await Blog.create({
+            title,
+            author,
+            text_body,
+            category_id,
+            post_date: new Date()
+        })
+
+        res.status(201).json(newBlogPost);
 
     } catch (err) {
-        console.log(err);
+        console.error('Error creating new blog post', err);
         res.status(500).json(err);
     }
 });
