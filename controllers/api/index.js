d732c934c25f5a3a70e5efb9be897bee113fa01e
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-06-15 08:18:03 -0500

    added functionality to delete blogs

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const blogFormRoutes = require('./blog-form-routes.js');
+const deleteBlog = require('./blog-delete.js');
+
 
 router.use('/users', userRoutes);
 router.use('/blog_form', blogFormRoutes);
+router.use('/blog_delete', deleteBlog);
 
 module.exports = router;
